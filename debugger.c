865613f8c7e8dbe7eafeee9d74273257a8967738
Andrew Plotkin <erkyrath@eblong.com>
2014-04-04 15:47:01 -0400

    Working on identifiers.

--- debugger.c
+++ debugger.c
@@ -15,8 +15,18 @@
 
 #include <libxml/xmlreader.h>
 
+typedef enum grouptype_enum {
+    grp_None = 0,
+    grp_Constant = 1,
+} grouptype;
+
 typedef struct xmlreadcontext_struct {
     strid_t str;
+    int failed;
+
+    grouptype curgrouptype;
+
+    xmlHashTablePtr identifiers;
 } xmlreadcontext;
 
 static int xmlreadfunc(void *rock, char *buffer, int len)
@@ -32,19 +42,18 @@ static int xmlclosefunc(void *rock)
 {
     xmlreadcontext *context = rock;
     glk_stream_close(context->str, NULL);
+    context->str = NULL;
     return 0;
 }
 
-static void xmlhandlenode(xmlTextReaderPtr reader)
+static void xmlhandlenode(xmlTextReaderPtr reader, xmlreadcontext *context)
 {
+    /*###
     const xmlChar *name, *value;
-
     name = xmlTextReaderConstName(reader);
     if (name == NULL)
         name = BAD_CAST "--";
-
     value = xmlTextReaderConstValue(reader);
-
     printf("%d %d %s %d %d",
         xmlTextReaderDepth(reader),
         xmlTextReaderNodeType(reader),
@@ -59,34 +68,86 @@ static void xmlhandlenode(xmlTextReaderPtr reader)
         else
             printf(" %s\n", value);
     }
+    ###*/
 
+    int depth = xmlTextReaderDepth(reader);
+    int nodetype = xmlTextReaderNodeType(reader);
+
+    if (depth == 0) {
+        if (nodetype == XML_ELEMENT_NODE) {
+            const xmlChar *name = xmlTextReaderConstName(reader);
+            if (xmlStrcmp(name, (xmlChar *)"inform-story-file")) {
+                printf("Error: This is not an Inform debug info file.\n"); /*###*/
+                context->failed = 1;
+            }
+        }
+        else if (nodetype == XML_ELEMENT_DECL) {
+            /* End of document */
+            context->curgrouptype = grp_None;
+        }
+    }
+    else if (depth == 1) {
+        if (nodetype == XML_ELEMENT_NODE) {
+            const xmlChar *name = xmlTextReaderConstName(reader);
+            if (xmlStrcmp(name, (xmlChar *)"identifier"))
+                context->curgrouptype = grp_Constant;
+            else
+                context->curgrouptype = grp_None;
+        }
+        else if (nodetype == XML_ELEMENT_DECL) {
+            /* End of group */
+            switch (context->curgrouptype) {
+            case grp_Constant:
+                if (context->identifierval && context->valueval) {
+                    printf("### constant %s, %s\n", context->identifierval, context->valueval);
+                }
+                break;
+            }
+            set_field_identifier(context, NULL);
+            set_field_value(context, NULL);
+        }
+    }
+    else {
+    }
 }
 
 void debugger_load_info(strid_t stream)
 {
-    xmlreadcontext context;
-    context.str = stream;
+    xmlreadcontext *context = (xmlreadcontext *)malloc(sizeof(xmlreadcontext));
+    context->str = stream;
+    context->failed = 0;
+    context->curgrouptype = grp_None;
+    context->identifiers = xmlHashCreate(16);
 
     xmlTextReaderPtr reader;
-    reader = xmlReaderForIO(xmlreadfunc, xmlclosefunc, &context, 
+    reader = xmlReaderForIO(xmlreadfunc, xmlclosefunc, context, 
         NULL, NULL, 
         XML_PARSE_RECOVER|XML_PARSE_NOENT|XML_PARSE_NONET|XML_PARSE_NOCDATA|XML_PARSE_COMPACT);
     if (!reader) {
-        return; /* error */
+        printf("Error: Unable to create XML reader.\n"); /*###*/
+        /*### free */
+        return;
     }
 
     while (1) {
         int res = xmlTextReaderRead(reader);
         if (res < 0) {
+            context->failed = 1;
             break; /* error */
         }
         if (res == 0) {
             break; /* EOF */
         }
-        xmlhandlenode(reader);
+        xmlhandlenode(reader, context);
     }
 
     xmlFreeTextReader(reader);
+
+    if (context->failed) {
+        printf("Error: Unable to load debug info.\n"); /*###*/
+        /*### free */
+        return;
+    }
 }
 
 #endif /* VM_DEBUGGER */
