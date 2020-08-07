dc293394c46aebfbcfd4a5d5a5906c8d8b2cefe1
Andrew Plotkin <zarf@ZarfLent.local>
2020-08-07 00:22:20 -0400

    Remove unneeded include.

--- unixstrt.c
+++ unixstrt.c
@@ -7,7 +7,6 @@
 #include <string.h>
 #include "glk.h"
 #include "gi_blorb.h"
-#include "gi_dispa.h"
 #include "glulxe.h"
 #include "unixstrt.h"
 #include "glkstart.h" /* This comes with the Glk library. */
