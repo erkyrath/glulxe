773d54a746947fe097cbfc1e17e1a50204c1abd0
Andrew Plotkin <zarf@ZarfLent.local>
2023-10-01 20:55:54 -0400

    Better state-seeding.

--- osdepend.c
+++ osdepend.c
@@ -200,11 +200,20 @@ static uint32_t mt_table[4];
 
 static void mt_seed_random(glui32 seed)
 {
-    //###
-    mt_table[0] = seed;
-    mt_table[1] = seed;
-    mt_table[2] = seed;
-    mt_table[3] = seed;
+    int ix;
+    /* We set up the 128-bit state using a different RNG, SplitMix32.
+       This isn't high-quality, but we just need to get a bunch of
+       bits into mt_table. */
+    for (ix=0; ix<4; ix++) {
+        seed += 2654435769;
+        glui32 s = seed;
+        s ^= s >> 16;
+        s *= 0x85ebca6b;
+        s ^= s >> 13;
+        s *= 0xc2b2ae35;
+        s ^= s >> 16;
+        mt_table[ix] = s;
+    }
 }
 
 static uint32_t rotl(const uint32_t x, int k) {
