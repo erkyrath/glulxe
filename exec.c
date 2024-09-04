633c4d6d104afbb2a3e477d9c5ebc184f9ba95fc
Andrew Plotkin <zarf@ZarfLent.local>
2024-09-04 11:39:30 -0400

    Check the -0x80000000/-1 case.

--- exec.c
+++ exec.c
@@ -119,6 +119,8 @@ void execute_loop()
         vals1 = inst[1].value;
         if (vals1 == 0)
           fatal_error("Division by zero.");
+        if (vals1 == -1 && (glui32)vals0 == 0x80000000)
+          fatal_error("Division overflow.");
         /* Since C doesn't guarantee the results of division of negative
            numbers, we carefully convert everything to positive values
            first. They have to be unsigned values, too, otherwise the
@@ -152,6 +154,8 @@ void execute_loop()
         vals1 = inst[1].value;
         if (vals1 == 0)
           fatal_error("Division by zero doing remainder.");
+        if (vals1 == -1 && (glui32)vals0 == 0x80000000)
+          fatal_error("Division overflow doing remainder.");
         if (vals1 < 0) {
             val1 = -(glui32)vals1;
         }
